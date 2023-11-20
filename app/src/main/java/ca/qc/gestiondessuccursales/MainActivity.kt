9866e50ef8888e6e036c897606744c31b545c6ef
Tristan Blond <68480964+Tantris514@users.noreply.github.com>
2023-11-19 22:24:24 -0500

    added btn to switch

--- app/src/main/java/ca/qc/gestiondessuccursales/MainActivity.kt
+++ app/src/main/java/ca/qc/gestiondessuccursales/MainActivity.kt
@@ -1,11 +1,25 @@
 package ca.qc.gestiondessuccursales
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Boutton Inscription
+        val btnInscription: Button = findViewById(R.id.btnInscription)
+
+
+        btnInscription.setOnClickListener {
+            val intent = Intent(this, inscription::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
