123b0b8d3876d2872364f059524b98ba4250a8c7
Tristan Blond <68480964+Tantris514@users.noreply.github.com>
2023-11-19 20:29:55 -0500

    added Inscrition activity

---
+++ app/src/main/java/ca/qc/gestiondessuccursales/inscription.kt
@@ -0,0 +1,11 @@
+package ca.qc.gestiondessuccursales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inscription : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription)
+    }
+}
